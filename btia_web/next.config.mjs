36dedcb19f741b45c20c9f555ecc0b0bed777595
= <=>
2024-04-21 20:33:00 +0900

    PWA 흔적 제거

--- btia_web/next.config.mjs
+++ btia_web/next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-import withSerwistInit from "@serwist/next";
-
 const config = {
     images: {
         remotePatterns: [
@@ -14,11 +11,4 @@ const config = {
     },
 };
 
-const withSerwist = withSerwistInit({
-    // Note: This is only an example. If you use Pages Router,
-    // use something else that works, such as "service-worker/index.ts".
-    swSrc: "app/sw.ts",
-    swDest: "public/sw.js",
-});
-
-export default withSerwist(config);
+export default config;
