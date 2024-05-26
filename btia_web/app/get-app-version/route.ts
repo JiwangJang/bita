e49b772d814e6bcfee1bfdfb15c39f2ba56f7bf2
= <=>
2024-05-26 16:53:21 +0900

    최신버전 수정

--- btia_web/app/get-app-version/route.ts
+++ btia_web/app/get-app-version/route.ts
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 
 export function GET() {
     // 앱 최신버전 입력
-    return NextResponse.json({ version: "0.1.0" });
+    return NextResponse.json({ version: "0.1.1" });
 }
