a12c765a5ba9e378b01fe974bc85252d41a9c739
Jang Jiwang <jiwang0917@naver.com>
2024-06-26 16:39:23 +0900

    광고 방식 변경

--- btia_app/lib/ad_helper.dart
+++ btia_app/lib/ad_helper.dart
@@ -1,38 +1,13 @@
 import 'dart:io';
 
 // 테스트용
-// class AdHelper {
-//   // 배너광고
-//   static String get bannerAdUnitId {
-//     if (Platform.isAndroid) {
-//       return 'ca-app-pub-3940256099942544/6300978111';
-//     } else if (Platform.isIOS) {
-//       return 'ca-app-pub-3940256099942544/2934735716';
-//     } else {
-//       throw UnsupportedError('Unsupported platform');
-//     }
-//   }
-
-//   // 전면광고
-//   static String get interstitialAdUnitId {
-//     if (Platform.isAndroid) {
-//       return "ca-app-pub-3940256099942544/1033173712";
-//     } else if (Platform.isIOS) {
-//       return "ca-app-pub-3940256099942544/4411468910";
-//     } else {
-//       throw UnsupportedError("Unsupported platform");
-//     }
-//   }
-// }
-
-// 실전용
 class AdHelper {
   // 배너광고
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-9383754596343414/2603516295';
+      return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-9383754596343414/1983024609';
+      return 'ca-app-pub-3940256099942544/2934735716';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -41,14 +16,39 @@ class AdHelper {
   // 전면광고
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-9383754596343414/4649211235";
+      return "ca-app-pub-3940256099942544/1033173712";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-9383754596343414/1903133551";
+      return "ca-app-pub-3940256099942544/4411468910";
     } else {
       throw UnsupportedError("Unsupported platform");
     }
   }
 }
+
+// 실전용
+// class AdHelper {
+//   // 배너광고
+//   static String get bannerAdUnitId {
+//     if (Platform.isAndroid) {
+//       return 'ca-app-pub-9383754596343414/2603516295';
+//     } else if (Platform.isIOS) {
+//       return 'ca-app-pub-9383754596343414/1983024609';
+//     } else {
+//       throw UnsupportedError('Unsupported platform');
+//     }
+//   }
+
+//   // 전면광고
+//   static String get interstitialAdUnitId {
+//     if (Platform.isAndroid) {
+//       return "ca-app-pub-9383754596343414/4649211235";
+//     } else if (Platform.isIOS) {
+//       return "ca-app-pub-9383754596343414/1903133551";
+//     } else {
+//       throw UnsupportedError("Unsupported platform");
+//     }
+//   }
+// }
 
 
 /**
--- btia_app/lib/view/photosWidget/upload_btn.dart
+++ btia_app/lib/view/photosWidget/upload_btn.dart
@@ -5,109 +5,79 @@ import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 
-class UploadBtn extends StatefulWidget {
+class UploadBtn extends StatelessWidget {
   const UploadBtn({required this.modalOn, super.key});
 
   final Function modalOn;
 
   @override
-  State<UploadBtn> createState() => _UploadBtnState();
-}
-
-class _UploadBtnState extends State<UploadBtn> {
-  InterstitialAd? _interstitialAd;
-
-  Future getInterstitialAdId() async {
-    InterstitialAd.load(
-      adUnitId: AdHelper.interstitialAdUnitId,
-      request: const AdRequest(),
-      adLoadCallback: InterstitialAdLoadCallback(
-        onAdLoaded: (ad) {
-          setState(() {
-            _interstitialAd = ad;
-          });
-        },
-        onAdFailedToLoad: (err) {
-          print('Failed to load an interstitial ad: ${err.message}');
-        },
-      ),
-    );
-  }
-
-  @override
-  void initState() {
-    getInterstitialAdId();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _interstitialAd!.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    print("build!");
     return Consumer<PhotosModel>(
       builder: (context, data, child) {
-        return AnimatedOpacity(
-          duration: const Duration(milliseconds: 500),
-          opacity: _interstitialAd != null ? 1 : 0.4,
-          child: Container(
-            height: 90,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    Color.fromRGBO(49, 51, 48, 1),
-                    Color.fromRGBO(49, 51, 48, 0)
-                  ]),
-            ),
-            child: Center(
-              child: CustomButton(
-                bgColor: const Color.fromRGBO(255, 107, 0, 1),
-                btnTitle: _interstitialAd != null ? "업로드" : "준비중",
-                onTapFunc: () async {
-                  if (data.isAdding || _interstitialAd == null) {
-                    return;
-                  }
+        return Container(
+          height: 90,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Color.fromRGBO(49, 51, 48, 1),
+                  Color.fromRGBO(49, 51, 48, 0)
+                ]),
+          ),
+          child: Center(
+            child: CustomButton(
+              bgColor: const Color.fromRGBO(255, 107, 0, 1),
+              btnTitle: "업로드",
+              onTapFunc: () async {
+                await InterstitialAd.load(
+                  adUnitId: AdHelper.interstitialAdUnitId,
+                  request: const AdRequest(),
+                  adLoadCallback: InterstitialAdLoadCallback(
+                    onAdLoaded: (ad) {
+                      ad.show();
+                    },
+                    onAdFailedToLoad: (err) {
+                      print(
+                          'Failed to load an interstitial ad: ${err.message}');
+                    },
+                  ),
+                );
 
-                  _interstitialAd?.show();
-                  getInterstitialAdId();
-                  Map<String, dynamic> result = await data.uploadImages();
+                if (data.isAdding) {
+                  return;
+                }
+                Map<String, dynamic> result = await data.uploadImages();
 
-                  late String title;
-                  late String content;
-                  if (result['success']) {
-                    title = '업로드 성공';
-                    content = '사이트에서 확인해보세요!';
-                  } else {
-                    switch (result['msg']) {
-                      case 'empty':
-                        content = '우선 촬영부터 해주세요';
-                        break;
-                      case 'network':
-                        content = '인터넷연결을 확인해주세요';
-                        break;
-                      case 'appErr':
-                        content = '개발자에게 문의해주세요';
-                        break;
-                      case 'serverErr':
-                        content = '잠시후 다시 시도해보세요';
-                        break;
-                      case 'tooLarge':
-                        content = '너무 커서 보내지 못한\n사진을 남겼습니다';
-                      default:
-                        content = '잠시후 다시 시도해보세요';
-                        break;
-                    }
-                    title = '업로드 실패';
+                late String title;
+                late String content;
+                if (result['success']) {
+                  title = '업로드 성공';
+                  content = '사이트에서 확인해보세요!';
+                } else {
+                  switch (result['msg']) {
+                    case 'empty':
+                      content = '우선 촬영부터 해주세요';
+                      break;
+                    case 'network':
+                      content = '인터넷연결을 확인해주세요';
+                      break;
+                    case 'appErr':
+                      content = '개발자에게 문의해주세요';
+                      break;
+                    case 'serverErr':
+                      content = '잠시후 다시 시도해보세요';
+                      break;
+                    case 'tooLarge':
+                      content = '너무 커서 보내지 못한\n사진을 남겼습니다';
+                    default:
+                      content = '잠시후 다시 시도해보세요';
+                      break;
                   }
-                  widget.modalOn(title, content);
-                },
-              ),
+                  title = '업로드 실패';
+                }
+                modalOn(title, content);
+              },
             ),
           ),
         );
